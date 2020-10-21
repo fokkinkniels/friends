a8e016964d5612b86be79d88aea9c82afedb33ce
fokkinkniels <fokkinkniels@gmail.com>
2020-10-21 13:33:45 +0200

    :fire: finished aws image upload project

--- Test-Projects/aws-image-upload/src/main/java/com/springdemo/awsimageupload/filestore/FileStore.java
+++ Test-Projects/aws-image-upload/src/main/java/com/springdemo/awsimageupload/filestore/FileStore.java
@@ -3,9 +3,13 @@ package com.springdemo.awsimageupload.filestore;
 import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
+import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.util.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 import java.util.Optional;
@@ -37,6 +41,15 @@ public class FileStore {
             s3.putObject(path, fileName, inputStream, metaData);
         } catch (AmazonServiceException e){
             throw new IllegalStateException("Failed to store file to s3 ", e);
+        }
+    }
+
+    public byte[] download(String path, String key) {
+        try{
+            S3Object object = s3.getObject(path, key);
+            return IOUtils.toByteArray(object.getObjectContent());
+        } catch (AmazonServiceException | IOException e){
+            throw new IllegalStateException("Failed to download to s3" + e);
         }
     }
 }
